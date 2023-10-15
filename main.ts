fc3ba29237cf1df19a55953416476a6893987389
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-15 16:20:28 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-function Schranke_öffnen () {
-    basic.showIcon(IconNames.ArrowNorth)
-    m = -100
+function Schranke_zu () {
+    basic.showIcon(IconNames.ArrowSouth)
+    m = 100
     motors.motorPower(m)
 }
 function Motor_aus () {
@@ -8,23 +8,23 @@ function Motor_aus () {
     m = 0
     motors.motorCommand(MotorCommand.Break)
 }
-function Schranke_schließen () {
-    basic.showIcon(IconNames.ArrowSouth)
-    m = 100
-    motors.motorPower(m)
-}
 function _ (Kommentar: string) {
 	
+}
+function Schranke_auf () {
+    basic.showIcon(IconNames.ArrowNorth)
+    m = -100
+    motors.motorPower(m)
 }
 let m = 0
 _("Schranke2LED")
 pins.digitalWritePin(DigitalPin.P0, 1)
 basic.pause(500)
-Schranke_schließen()
+Schranke_zu()
 basic.forever(function () {
     if (m == 0 && pins.analogReadPin(AnalogPin.P1) > 60) {
         _("Motor aus und Lichtschranke dunkel -> Schranke öffnen")
-        Schranke_öffnen()
+        Schranke_auf()
     } else if (m < 0 && input.pinIsPressed(TouchPin.P2)) {
         _("Motor öffnet Schranke (m < 0) und Schranke ist ganz offen (P2) -> Motor aus")
         Motor_aus()
@@ -33,7 +33,7 @@ basic.forever(function () {
             basic.showNumber(Index)
             basic.pause(1000)
         }
-        Schranke_schließen()
+        Schranke_zu()
     } else if (m > 0 && input.pinIsPressed(TouchPin.P3)) {
         _("Motor schließt Schranke (m > 0) und Schranke ist ganz geschlossen (P3) -> Motor aus")
         Motor_aus()
